34df1317eda7eb3c6507a544afb2b1458157517f
VengeBiKay <veng.y@bi-kay.com>
2020-10-13 11:24:41 +0700

    change image on news detail

--- App/Containers/NewsDetailScreen.js
+++ App/Containers/NewsDetailScreen.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {View,Text,ScrollView,} from 'react-native';
+import {View,Text,ScrollView,Image} from 'react-native';
 import Loading from '../Components/Loading'
 
 export default class NewsDetailScreen extends Component{
@@ -7,7 +7,8 @@ export default class NewsDetailScreen extends Component{
         super(props);
         this.state={
             statusLoading:true,
-            content:props.item.content
+            content:props.item.content,
+            filePath:props.item.filePath,
         }
     }
 
@@ -18,16 +19,24 @@ export default class NewsDetailScreen extends Component{
     }
 
     render(){
-        const {content,statusLoading}=this.state
+        const {content,statusLoading,filePath}=this.state
         if (statusLoading) return <Loading />
 
         return(
             <View style={{flex:1}}>
                 <ScrollView>
                     <View style={{ justifyContent: 'center', padding: 10 }}>
-                        <Text style={{ fontSize: 15, paddingTop: 10,paddingLeft:10,lineHeight:24 }}>
-                            {content}
-                        </Text>
+                       
+                            <View style={{ width: '100%', justifyContent: 'center' }}>
+                                <Image source={{ uri: "http://192.168.0.100:3000"+filePath }} style={{ width: '90%', height: 250, alignSelf: 'center' }} />
+                            <View>
+                            </View>  
+                            <Text style={{ fontSize: 15, paddingTop: 10,paddingLeft:10,lineHeight:24 }}>
+                                  {content}
+                             </Text>
+                           </View>
+                       
+                        
                     </View>
                </ScrollView>
             </View>
